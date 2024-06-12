6e14acafbf21fc0584c5a8bdc08b8c3a3a3a83ac
javalladares <javalladares@google.com>
2024-06-12 18:38:30 +0000

    [fix]: fully skip priority Q for non-ordered pcaps

--- pcap-writer/pkg/transformer/transformer.go
+++ pcap-writer/pkg/transformer/transformer.go
@@ -23,7 +23,7 @@ type PcapTranslator interface {
 
 type PcapTransformer struct {
 	ctx            context.Context
-	ich            chan<- concurrently.WorkFunction
+	ich            chan concurrently.WorkFunction
 	och            <-chan concurrently.OrderedOutput
 	translator     PcapTranslator
 	translatorPool *ants.PoolWithFunc
@@ -89,6 +89,7 @@ func (w pcapWorker) translateTCPLayer(ctx context.Context, buffer *fmt.Stringer)
 func (w pcapWorker) Run(ctx context.Context) interface{} {
 	buffer := w.translator.next(ctx, w.serial)
 
+	// [TODO]: translate layers concurrently
 	w.translateEthernetLayer(ctx, &buffer)
 	w.translateIPLayer(ctx, &buffer)
 	w.translateTCPLayer(ctx, &buffer)
@@ -98,40 +99,38 @@ func (w pcapWorker) Run(ctx context.Context) interface{} {
 	return &buffer
 }
 
-func (t *PcapTransformer) translatePacket(ctx context.Context, task *pcapWorker) error {
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	case t.ich <- task:
-		t.wg.Add(len(t.writers))
-	}
-	return nil
-}
-
-func (t *PcapTransformer) produceTranslations(ctx context.Context) {
-	for translation := range t.och {
-		// consume translations and push them into translations consumers
-		for _, translations := range t.writeQueues {
-			// if any of the consumers' buffers is full,
-			// the saturated/slower one will block and delay iterations.
-			// Blocking is more likely when `preserveOrder` is enabled; however,
-			// if `preserveOrder` is disabled, non written translations are dropped after 5s.
-			translations <- translation.Value.(*fmt.Stringer)
-		}
-	}
-}
-
 func (t *PcapTransformer) writeTranslation(ctx context.Context, task *pcapWriteTask) {
 	// consume translations – flush them into writers
 	io.WriteString(task.writer, fmt.Sprintf("%s\n", *task.translation))
 	t.wg.Done()
 }
 
+func (t *PcapTransformer) publishTranslation(ctx context.Context, translation *fmt.Stringer) {
+	for _, translations := range t.writeQueues {
+		// if any of the consumers' buffers is full,
+		// the saturated/slower one will block and delay iterations.
+		// Blocking is more likely when `preserveOrder` is enabled.
+		translations <- translation
+	}
+}
+
+func (t *PcapTransformer) produceTranslation(ctx context.Context, task *pcapWorker) error {
+	t.publishTranslation(ctx, task.Run(ctx).(*fmt.Stringer))
+	return nil
+}
+
+func (t *PcapTransformer) produceTranslations(ctx context.Context) {
+	// translations are made available in the enqueued order
+	for translation := range t.och {
+		// consume translations and push them into translations consumers
+		t.publishTranslation(ctx, translation.Value.(*fmt.Stringer))
+	}
+}
+
 func (t *PcapTransformer) consumeTranslations(ctx context.Context, index int) {
-	var translations chan *fmt.Stringer = t.writeQueues[index]
 	var writer io.Writer = t.writers[index]
 
-	for translation := range translations {
+	for translation := range t.writeQueues[index] {
 
 		task := &pcapWriteTask{
 			ctx:         ctx,
@@ -140,21 +139,20 @@ func (t *PcapTransformer) consumeTranslations(ctx context.Context, index int) {
 		}
 
 		if t.preserveOrder {
+			// this is mostly blocking
 			t.writeTranslation(ctx, task)
 		} else {
+			// this is mostly non-blocking
 			t.writerPool.Invoke(task)
 		}
-
 	}
 }
 
-func (t *PcapTransformer) waitForContextDone(ctx context.Context) error {
+func (t *PcapTransformer) waitForContextDone(ctx context.Context) {
 	select {
 	case <-ctx.Done():
 		close(t.ich)
-		return ctx.Err()
 	}
-	return nil
 }
 
 // returns when all packets have been transformed and written
@@ -166,22 +164,13 @@ func (t *PcapTransformer) WaitDone() {
 	// if order is not enforced, there are not worker pools to be stopped
 	if !t.preserveOrder {
 		t.translatorPool.Release()
-		t.writerPool.ReleaseTimeout(0 * time.Second)
+		t.writerPool.ReleaseTimeout(0)
 	}
 }
 
 func (t *PcapTransformer) Apply(ctx context.Context, packet *gopacket.Packet, serial *int64) error {
-	// process/translate packets concurrently in order to
-	// avoid blocking `gopacket` captured packets channel.
-	// Order of gorouting execution is not guaranteed, which means
-	// that packets will be consumed/written in non-deterministic order.
-	// `serial` is aviailable to be used for sorting PCAP files.
-
-	select {
-	case <-ctx.Done():
-		return ctx.Err()
-	default:
-	}
+	// It is assumed that packets will be produced faster than translations.
+	// process/translate packets concurrently in order to avoid blocking `gopacket` packets channel.
 
 	task := &pcapWorker{
 		serial:     serial,
@@ -189,22 +178,28 @@ func (t *PcapTransformer) Apply(ctx context.Context, packet *gopacket.Packet, se
 		translator: t.translator,
 	}
 
+	t.wg.Add(len(t.writers))
+
+	// If ordered output is enabled, enqueue translation workers in packet capture order;
+	// this will introduce some level of contention as the translation Q starts to fill (saturation):
+	// if the next packet to arrive finds a full Q, this method will block until slots are available.
+	// The degree of contention is proportial to the Q capacity times translation latency.
+	// Order should only be used for not network intersive workloads.
 	if t.preserveOrder {
-		select {
-		case <-ctx.Done():
-			return ctx.Err()
-		case t.ich <- task:
-			t.wg.Add(len(t.writers))
-		}
+		t.ich <- task
 		return nil
 	}
 
+	// if ordered output is disabled, translate packets concurrently via translator pool.
+	// Order of gorouting execution is not guaranteed, which means
+	// that packets will be consumed/written in non-deterministic order.
+	// `serial` is aviailable to be used for sorting PCAP files.
 	t.translatorPool.Invoke(task)
 	return nil
 }
 
 func translatePacket(ctx context.Context, transformer *PcapTransformer, worker interface{}) {
-	transformer.translatePacket(ctx, worker.(*pcapWorker))
+	transformer.produceTranslation(ctx, worker.(*pcapWorker))
 }
 
 func writeTranslation(ctx context.Context, transformer *PcapTransformer, task interface{}) {
@@ -224,6 +219,9 @@ func newTranslator(format *string) (PcapTranslator, error) {
 	return nil, fmt.Errorf("translator unavailable: %s", *format)
 }
 
+// if preserving packet capture order is not required, translations may be done concurrently
+// concurrently translating packets means that translations are not enqueded in packet capture order.
+// Similarly, sinking translations into files can be safely done concurrently ( in whatever order goroutines are scheduled )
 func provideWorkerPools(ctx context.Context, transformer *PcapTransformer, numWriters int) {
 	poolOpts := ants.Options{
 		PreAlloc:       true,
@@ -232,17 +230,31 @@ func provideWorkerPools(ctx context.Context, transformer *PcapTransformer, numWr
 	}
 	poolOpt := ants.WithOptions(poolOpts)
 
+	poolSize := 10 * numWriters
+
 	translatorPoolFn := func(i interface{}) {
 		translatePacket(ctx, transformer, i)
 	}
-	translatorPool, _ := ants.NewPoolWithFunc(10, translatorPoolFn, poolOpt)
+	translatorPool, _ := ants.NewPoolWithFunc(poolSize, translatorPoolFn, poolOpt)
 	transformer.translatorPool = translatorPool
 
 	writerPoolFn := func(i interface{}) {
 		writeTranslation(ctx, transformer, i)
 	}
-	writerPool, _ := ants.NewMultiPoolWithFunc(numWriters, 10*numWriters, writerPoolFn, ants.LeastTasks, poolOpt)
+	writerPool, _ := ants.NewMultiPoolWithFunc(numWriters, poolSize, writerPoolFn, ants.LeastTasks, poolOpt)
 	transformer.writerPool = writerPool
+}
+
+func provideConcurrentQueue(ctx context.Context, transformer *PcapTransformer, numWriters int) {
+	queueSize := 20 * numWriters
+
+	ochOpts := &concurrently.Options{
+		PoolSize:         queueSize,
+		OutChannelBuffer: queueSize,
+	}
+
+	transformer.ich = make(chan concurrently.WorkFunction, queueSize)
+	transformer.och = concurrently.Process(ctx, transformer.ich, ochOpts)
 }
 
 // transformers get instances of `io.Writer` instead of `pcap.PcapWriter` to prevent closing.
@@ -252,15 +264,11 @@ func newTransformer(ctx context.Context, writers []io.Writer, format *string, pr
 		return nil, err
 	}
 
-	ich := make(chan concurrently.WorkFunction, 10)
-	ochOpts := &concurrently.Options{PoolSize: 10, OutChannelBuffer: 10}
-	och := concurrently.Process(ctx, ich, ochOpts)
-
 	numWriters := len(writers)
 	// not using `io.MultiWriter` as it writes to all writers sequentially
 	writeQueues := make([]chan *fmt.Stringer, numWriters)
 	for i := range writers {
-		writeQueues[i] = make(chan *fmt.Stringer, 10)
+		writeQueues[i] = make(chan *fmt.Stringer, 10*numWriters)
 	}
 
 	var wg sync.WaitGroup
@@ -270,27 +278,27 @@ func newTransformer(ctx context.Context, writers []io.Writer, format *string, pr
 	transformer := &PcapTransformer{
 		wg:            wg,
 		ctx:           ctx,
-		ich:           ich,
-		och:           och,
 		translator:    translator,
 		writers:       writers,
 		writeQueues:   writeQueues,
 		preserveOrder: preserveOrder,
 	}
 
-	// `preserveOrder==true` causes writes to be sequential and blocking per writer.
-	// `preserveOrder==true` does not cause `transformer.Apply` to block.
-	if !preserveOrder {
+	// `preserveOrder==true` causes writes to be sequential and blocking per `io.Writer`.
+	// `preserveOrder==true` although blocking at writting, does not cause `transformer.Apply` to block.
+	if preserveOrder {
+		provideConcurrentQueue(ctx, transformer, numWriters)
+		go transformer.waitForContextDone(ctx)
+	} else {
 		provideWorkerPools(ctx, transformer, numWriters)
 	}
 
 	go transformer.produceTranslations(ctx)
-	// spawn consumers for all writers
+	// spawn consumers for all `io.Writer`s
+	// 1 consumer goroutine per `io.Writer`
 	for i := range writeQueues {
 		go transformer.consumeTranslations(ctx, i)
 	}
-
-	go transformer.waitForContextDone(ctx)
 
 	return transformer, nil
 }
